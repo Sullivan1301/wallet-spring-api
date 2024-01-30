8b6035978f0d880ad17bf7049dadf41328291a49
Sullivan1301 <jorosullivan13@gmail.com>
2024-01-30 22:32:58 +0300

    feat controller for having notion with postman, it's not really for the Wallet

---
+++ src/main/java/org/example/walletapi/controller/HelloController.java
@@ -0,0 +1,37 @@
+package org.example.walletapi.controller;
+//This is a controller for having notion with postman, it's not really for the Wallet
+
+import org.springframework.web.bind.annotation.*;
+
+public class HelloController {
+    @GetMapping("/account/{id}")
+    public String accountById(@PathVariable(name = "id") String accountId) {
+        return "Account ID = " + accountId;
+    }
+
+    @GetMapping("/hello")
+    public String ping(@RequestParam(required = false) Integer page,
+                       @RequestParam(required = false) Integer size) {
+        return "This is a GET request with params page = " + page + " and size=" + size;
+    }
+
+    @PostMapping("/hello")
+    public String postHello(@RequestBody(required = false) String input) {
+        return "This is a POST request and this is the input : " + input;
+    }
+
+    @PutMapping("/hello")
+    public String putHello() {
+        return "This is a PUT request";
+    }
+
+    @PatchMapping("/hello")
+    public String patchtHello() {
+        return "This is a PATCH request";
+    }
+
+    @DeleteMapping("/hello")
+    public String deletetHello() {
+        return "This is a DELETE request";
+    }
+}
