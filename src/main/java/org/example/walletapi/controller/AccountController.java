8d12f3a2b8ffaebcdd04227e0bff25b3c08592e7
Sullivan1301 <jorosullivan13@gmail.com>
2024-01-30 22:22:55 +0300

    feat: write AccountController following example of Mr Ryan

--- src/main/java/org/example/walletapi/controller/AccountController.java
+++ src/main/java/org/example/walletapi/controller/AccountController.java
@@ -1,8 +1,22 @@
 package org.example.walletapi.controller;
 
+import lombok.AllArgsConstructor;
 import org.example.walletapi.DAO.AccountDAO;
+import org.example.walletapi.model.Account;
 import org.example.walletapi.service.AccountService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
 public class AccountController {
+    private final AccountService accountService;
+
+    @GetMapping("/accounts")
+    public List<Account> getAccounts(){
+        return accountService.getAccounts();
+    }
 }
 
